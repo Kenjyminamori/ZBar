ee766a2ab9bf05b324a4f69533792c2c4019c6f2
Roman Savrulin <savrulin@flexibity.com>
2016-08-19 18:54:48 +0300

    ignore optimized build files

--- configure-for-debug.sh
+++ configure-for-debug.sh
@@ -3,4 +3,4 @@
 autoreconf --install
 mkdir out
 cd out
-../configure --without-imagemagick --without-python --without-qt --with-jpeg 'CFLAGS=-O0 -g'
+../configure --without-python --without-qt --with-jpeg 'CFLAGS=-O0 -g'
